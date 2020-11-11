cb2dd02c1e50ba5dedc38e4abf02f7be87f38202
xyzkljl1 <xyzkljl1@yahoo.com>
2020-11-11 16:54:44 +0800

    优化重命名

--- server.py
+++ server.py
@@ -5,6 +5,7 @@ import subprocess
 import re
 import time
 import database
+import locale
 import _thread
 sys.path.append('./lib/')
 import youtube_dl
@@ -12,41 +13,39 @@ download_dir="D:/VideoDownload/"
 
 def Run():
     http_server=http.server.HTTPServer(('localhost',4000),HTTPHandler)
-    _thread.start_new_thread(Processor)
+    _thread.start_new_thread(Processor,())
     http_server.serve_forever()
 
 def Processor():
     while True:
         queue=database.GetQueue()
         for (id,url) in queue:
+            print('Try Start Task {0}:{1}'.format(id,url))
             #get title
             res = urllib.parse.urlparse(url)
             dir =os.path.join(download_dir,res.hostname.split('.')[-2])
             cmd = GetCmd(url, res.hostname)
             cmd.append('-e')
-            p = subprocess.Popen(
-                cmd, stderr=subprocess.PIPE, stdout=subprocess.PIPE)
-            title, stderr = p.communicate()
-            if p.returncode != 0:
-                print(stderr.decode('utf-8', 'replace'))
-                continue
-            name = re.sub("[\/\\\:\*\?\"\<\>\|]", "_", title[:-1].decode()) + "[{0}]".format(id)
-            path=os.path.join(dir,name)
+
             if not os.path.exists(dir):
                 os.makedirs(dir)
-            if os.path.exists(path):
-                os.remove(path)
             #download
             cmd = GetCmd(url, res.hostname)
-            cmd.append(["-o","\"{0}\"".format(path)])
+            cmd.append(["-o","\"{0}/%(title)s[{1}].%(ext)s\"".format(dir,id)])
+            """
+            ！！：需要在pycharm的setting的File Encodings里把encoding设置成System Default
+            不设置Popen的encoding时返回bytes，设置后返回str，但是实际上只是对bytes做了decode而已，并不会更改进程实际返回的内容，设置的encoding不正确时会抛出异常
+            使用pycharm的运行/调试运行时，实际编码使用pycharm的设置，而locale.getdefaultlocale()的值并不会随之变化，十分坑爹
+            为了避免代码随IDE配置变化，只能将其设为System Default
+            """
             p = subprocess.Popen(
                 cmd, stderr=subprocess.PIPE, stdout=subprocess.PIPE)
             _, stderr = p.communicate()
             if p.returncode != 0:
-                print(stderr.decode('utf-8', 'replace'))
-                continue
-            if os.path.exists(path):
+                print('Fail',id,stderr,stderr.decode(locale.getpreferredencoding()))
+            else:
                 database.RemoveRow(id)
+                print('Success', id)
         time.sleep(30)
 
 def GetCmd(url:str,hostname):
@@ -55,9 +54,7 @@ def GetCmd(url:str,hostname):
                 "--no-playlist",
                 url]
     if 'youtube' in hostname or 'xnxx' in hostname or 'xvideos' in hostname:
-        cmd.append(["--proxy", "127.0.0.1:1081"])
-    else:
-        cmd.append([])
+        cmd.append(["--proxy=127.0.0.1:1081"])
     return cmd
 
 class HTTPHandler(http.server.BaseHTTPRequestHandler):
