e7289f6fe4f4fc73afc50005ae151680f5485977
xyzkljl1 <xyzkljl1@yahoo.com>
2022-07-14 20:21:35 +0800

    update nhentai lib

--- downloader_nhentai.py
+++ downloader_nhentai.py
@@ -11,7 +11,7 @@ def Download(url,hostname,cookie,dir,proxy_a,proxy_b):
     try:
         res=urllib.parse.urlparse(url)
         id=res.path.split('/')[2]
-        lib.nhentai.command.main(proxy_a,cookie,["--id="+id,"--format","[%a](%i)%s","-o",dir,"--no-html"])
+        lib.nhentai.command.main(proxy_a,cookie,["--id="+id,"--format","[%a](%i)%s","-o",dir,"--no-html","--useragent","Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36"])
         return True,""
     except Exception as e:
         import traceback
